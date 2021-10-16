91c4f0b3005d0d6df5ef0d04e3789feaec35aa01
danilo <dsalinux@gmail.com>
2021-10-16 16:37:55 -0300

    Implememtação de relacionamento com hibernate.

--- src/main/java/br/edu/iftm/ecommerce/model/Usuario.java
+++ src/main/java/br/edu/iftm/ecommerce/model/Usuario.java
@@ -2,11 +2,15 @@ package br.edu.iftm.ecommerce.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -37,5 +41,12 @@ public class Usuario implements Serializable {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name="data_cadastro")
     private Date dataCadastro;
+    
+    @ManyToMany
+    @JoinTable(name = "usuario_permissao",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "permissao_id")
+    )
+    private List<Permissao> permissoes;
     
 }
