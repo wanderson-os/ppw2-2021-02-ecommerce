7d07e1227e8de14e8cc4c20ab78c2de4912fbd28
danilo <dsalinux@gmail.com>
2021-10-23 18:29:39 -0300

    Adição de template admin responsivo.

---
+++ src/main/webapp/resources/ecommerceif/js/script.js
@@ -0,0 +1,16 @@
+document.getElementsByClassName('show-menu')[0].addEventListener("click", function(){
+    if(isMobile()){
+        document.getElementsByClassName('wrapper')[0].classList.toggle('active-menu-mobile');
+    } else {
+        document.getElementsByClassName('wrapper')[0].classList.toggle('active-menu');
+    }
+});
+document.getElementsByClassName('sidebar-overlay')[0].addEventListener("click", function(){
+    document.getElementsByClassName('wrapper')[0].classList.remove('active-menu-mobile');
+});
+/*
+*   Check if screen size is less than 768px
+*/
+function isMobile() {
+    return window.matchMedia("(max-width: 768px)").matches;
+}
