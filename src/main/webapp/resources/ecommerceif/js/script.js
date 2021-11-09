36908f16268cb1eaddebaa513217d50c1a0c39aa
danilo <dsalinux@gmail.com>
2021-11-09 18:52:34 -0300

    Implementação de icones Fontawsome.

--- src/main/webapp/resources/ecommerceif/js/script.js
+++ src/main/webapp/resources/ecommerceif/js/script.js
@@ -1,16 +1,27 @@
-document.getElementsByClassName('show-menu')[0].addEventListener("click", function(){
-    if(isMobile()){
+document.getElementsByClassName('show-menu')[0].addEventListener("click", function () {
+    if (isMobile()) {
         document.getElementsByClassName('wrapper')[0].classList.toggle('active-menu-mobile');
     } else {
         document.getElementsByClassName('wrapper')[0].classList.toggle('active-menu');
     }
 });
-document.getElementsByClassName('sidebar-overlay')[0].addEventListener("click", function(){
+document.getElementsByClassName('sidebar-overlay')[0].addEventListener("click", function () {
     document.getElementsByClassName('wrapper')[0].classList.remove('active-menu-mobile');
 });
 /*
-*   Check if screen size is less than 768px
-*/
+ *   Check if screen size is less than 768px
+ */
 function isMobile() {
     return window.matchMedia("(max-width: 768px)").matches;
 }
+
+document.querySelectorAll('.submenu').forEach(function (element, index, array) {
+    element.addEventListener("click",
+            function () {
+                this.classList.toggle('submenu-active');
+                console.log(this)
+            }
+    );
+
+}
+);
